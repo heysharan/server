9e073605a25cbe217b5dd0b73ef6f0cb11a77b02
Sharan C M <cmsharan.2019@gmail.com>
2025-08-07 21:12:22 +0530

    ping server

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,62 @@
+// import express from 'express'
+// import cors from 'cors'
+
+// const app = express();
+// app.use(cors());
+
+// interface Todo {
+//     id: number,
+//     title: string,
+//     description: string,
+//     completed: boolean
+// }
+
+// let todos: Todo[] = []
+
+// const generateTodos = (count: number) => {
+//     todos = Array.from({ length: count }, (_, idx) => {
+//         const n = idx + 1;
+//         return {
+//             id: n,
+//             title: `Todo ${n}`,
+//             description: `This is Todo ${n}`,
+//             completed: n % 2 === 0 ? true : false
+//         }
+//     })
+// }
+
+
+// let count: number
+// let lastcount: number = -1
+
+// app.get('/todos', (req, res) => {
+
+//     do {
+//         count = Math.floor(Math.random() * 100)
+//     } while (count === lastcount)
+//     lastcount = count
+
+//     generateTodos(count)
+//     res.json({
+//         count: count,
+//         todos: todos,
+//     })
+// })
+
+// app.listen(3000, () => {
+//     console.log("Server running on PORT 3000")
+// })
+
+
 import express from 'express'
 import cors from 'cors'
 
 const app = express();
-app.use(cors());
 
-interface Todo {
-    id: number,
-    title: string,
-    description: string,
-    completed: boolean
-}
+app.use(cors())
 
-let todos: Todo[] = []
-
-const generateTodos = (count: number) => {
-    todos = Array.from({ length: count }, (_, idx) => {
-        const n = idx + 1;
-        return {
-            id: n,
-            title: `Todo ${n}`,
-            description: `This is Todo ${n}`,
-            completed: n % 2 === 0 ? true : false
-        }
-    })
-}
-
-
-let count: number
-let lastcount: number = -1
-
-app.get('/todos', (req, res) => {
-
-    do {
-        count = Math.floor(Math.random() * 100)
-    } while (count === lastcount)
-    lastcount = count
-
-    generateTodos(count)
-    res.json({
-        count: count,
-        todos: todos,
-    })
+app.get('/ping', (req, res) => {
+    res.send(200)
 })
 
 app.listen(3000, () => {
